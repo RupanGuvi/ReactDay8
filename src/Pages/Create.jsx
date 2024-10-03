5f15626d6f86f20856f8d5dedd2350b855acb447
RupanGuvi <rupanguvi@gmail.com>
2024-10-03 20:03:52 +0530

    create and get completed

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,11 +1,134 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-    return (
-        <div>
-            <h1>Create Page</h1>
+  const [formData, setFormData] = useState({
+    productId: "",
+    productName: "",
+    productImage: "",
+    productPrice: "",
+    productDescription: "",
+  });
+const navigate = useNavigate();
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+        await axios.post("https://66fe99a02b9aac9c997ca10e.mockapi.io/api/v1/products",formData);
+        navigate("/")
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <label
+            htmlFor="productId"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Product Id
+          </label>
+          <input
+            type="text"
+            id="productId"
+            name="productId"
+            onChange={handleChange}
+            value={formData.productId}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Your Product Id"
+            required
+          />
         </div>
-    );
+        <div className="mb-5">
+          <label
+            htmlFor="productName"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Product Name
+          </label>
+          <input
+            type="text"
+            id="productName"
+            name="productName"
+            value={formData.productName}
+            onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Your Product Name"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="productImage"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Product Image
+          </label>
+          <input
+            type="text"
+            id="productImage"
+            name="productImage"
+            value={formData.productImage}
+            onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Your Product Image URL"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="productPrice"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Product Price
+          </label>
+          <input
+            type="text"
+            id="productPrice"
+            name="productPrice"
+            value={formData.productPrice}
+            onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Your Product Price"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="productDescription"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Product Description
+          </label>
+          <input
+            type="text"
+            id="productDescription"
+            name="productDescription"
+            value={formData.productDescription}
+            onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Your Product Description"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,11 +17,11 @@ const Home = () => {
   //console.log(data);
 
   return (
-    <div>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        {data.map((ele) => {
-          return (
-            <div key={ele.productId}>
+    <div className="flex flex-1 flex-wrap gap-6">
+      {data.map((ele) => {
+        return (
+          <div key={ele.productId}>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {ele.productId}
               </h2>
@@ -35,7 +35,7 @@ const Home = () => {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   {ele.productName}
                 </h5>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="mb-2 text-xl  tracking-tight text-gray-900 dark:text-white">
                   {ele.productPrice}
                 </h5>
 
@@ -44,23 +44,25 @@ const Home = () => {
                 </p>
               </div>
               <div className="text-center">
-                <button
-                  type="button"
-                  class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                >
-                  Edit
-                </button>
-                <button
-                  type="button"
-                  class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                >
-                  Delete
-                </button>
+                <div>
+                  <button
+                    type="button"
+                    className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
